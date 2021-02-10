e0db289f5f7c841bd0e834769aab0d77aaadc612
Elias Calagiu <eliasc81@gmail.com>
2021-02-09 20:51:38 -0800

    initial file setup and server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const compression = require("compression")
+const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+app.use(compression());
+
+
+app.get('/', (req,res)=>{
+    res.send("working")
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on: " + PORT);
+});
